ca4c79eaa96f4b9c6358dc0a564a131c95bdaf19
balov-oa <balov-oa@yandex.ru>
2020-05-24 16:17:52 +0700

    Change to db connect

--- Downloader.py
+++ Downloader.py
@@ -1,13 +1,20 @@
-from os import listdir
 import requests
 from bs4 import BeautifulSoup
-from pathlib import Path
-import json
+import urllib
+import sqlalchemy
+import pandas as pd
+from tqdm import tqdm
+
+params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};"
+                                 "SERVER=OLEG;"
+                                 "DATABASE=Apartment_Tomsk;"
+                                 "Trusted_Connection=yes")
+engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect={0}".format(params))
 
 
 def get_soup_by_url(url):
-    html_ = requests.get(url).text
-    soup = BeautifulSoup(html_, 'lxml')
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, 'lxml')
 
     return soup
 
@@ -15,7 +22,8 @@ def get_soup_by_url(url):
 # Получаем номер последней страницы
 def get_number_last_page():
     soup = get_soup_by_url(
-        'https://www.tomsk.ru09.ru/realty?type=1&otype=1&district[1]=on&district[2]=on&district[3]=on&district[4]=on&perpage=50&page=1')
+        'https://www.tomsk.ru09.ru/'
+        'realty?type=1&otype=1&district[1]=on&district[2]=on&district[3]=on&district[4]=on&perpage=50&page=1')
     number_last_page = int(soup.find('td', {'class': 'pager_pages'}).find_all('a')[4].text)
 
     return number_last_page
@@ -29,9 +37,6 @@ def find_district_field(keys):
 
 
 def parse_apartment(url):
-    #     headers = {'User-Agent: Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'}
-    #     start_time = time.time()
-
     soup = get_soup_by_url(url)
 
     keys = [i.find('span').text.replace('\xa0', '').lower() for i in
@@ -51,12 +56,14 @@ def parse_apartment(url):
     items['ид'] = int(soup.find('strong').text)
     items['дата добавления'] = soup.find(class_='realty_detail_date nobr').get('title')
     items['дата истечения'] = soup.find_all(class_='realty_detail_date')[4].get('title')
+    items['ссылка'] = url
 
     return items
 
 
 def get_urls_pages(start_page=1, end_page=None):
-    url_base = 'https://www.tomsk.ru09.ru/realty?type=1&otype=1&district[1]=on&district[2]=on&district[3]=on&district[4]=on&perpage=50&page='
+    url_base = 'https://www.tomsk.ru09.ru/' \
+               'realty?type=1&otype=1&district[1]=on&district[2]=on&district[3]=on&district[4]=on&perpage=50&page='
 
     end_page = end_page or get_number_last_page()
     pages_to_parse = range(start_page, end_page + 1)
@@ -71,48 +78,54 @@ def get_urls_apartments_by_page(url_page):
     soup = get_soup_by_url(url_page)
     soup = soup.find_all('a', {'class': 'visited_ads'})
 
-    urls_apartments = set()
-
-    for i in soup:
-        urls_apartments.add(url_base + i.get('href'))
+    urls_apartments = {url_base + i.get('href') for i in soup}
 
     return urls_apartments
 
 
-def main(start_page=1, end_page=None, filename='data.json'):
-    base_path = Path(__file__).parent.parent.joinpath('Data')
-    full_filename = base_path.joinpath(filename)
+def main(start_page=1, end_page=None):
+    rename_map = {'район': 'District',
+                  'адрес': 'Address',
+                  'вид': 'Sales_Type',
+                  'год постройки': 'Year_Building',
+                  'материал': 'Material',
+                  'этаж/этажность': 'Floor_Numbers_Of_Floors',
+                  'этажность': 'Floors_In_Building',
+                  'тип квартиры': 'Apartment_Type',
+                  'цена': 'Price',
+                  'общая площадь': 'Square_Total',
+                  'жилая': 'Square_Living',
+                  'кухня': 'Square_Kitchen',
+                  'количество комнат': 'Rooms_Number',
+                  'отделка': 'Apartment_Condition',
+                  'санузел': 'Bathroom_Type',
+                  'балкон/лоджия': 'Balcony_Loggia',
+                  'дата добавления': 'Date_Add',
+                  'дата истечения': 'Date_Expiration',
+                  'ссылка': 'Url_Link',
+                  'ид': 'Id'}
 
+    df = pd.DataFrame(columns=list(rename_map.keys()))
 
-    if filename in listdir(base_path):
-        with open(full_filename, 'r') as file:
-            storage_dict = json.load(file)
-    else:
-        storage_dict = {}
+    urls_in_database = pd.read_sql('SELECT DISTINCT Url_Link FROM Apartment_Tomsk.dbo.Apartments', engine)
+    urls_in_database = set(urls_in_database['Url_Link'])
 
-    len_storage = len(storage_dict)
+    len_storage = len(urls_in_database)
     print('Apartments in storage:', len_storage, '\n')
 
     urls_pages = get_urls_pages(start_page, end_page)
-
-    # for url_page in tqdm(urls_pages, desc='Pages', position=1, leave=False):
-    for i, url_page in enumerate(urls_pages):
-
+    for url_page in tqdm(urls_pages, desc='Pages', leave=False, ascii=True, ):
         urls_apartments = get_urls_apartments_by_page(url_page)
-        urls_apartments_to_parse = urls_apartments.difference(set(storage_dict))
-        print('Pages {0} of {1} - Count:{2}'.format(i, len(urls_pages), len(urls_apartments_to_parse)))
+        urls_apartments_to_parse = urls_apartments.difference(urls_in_database)
 
         if len(urls_apartments_to_parse) != 0:
-            # for url_apartment in tqdm(urls_apartments_to_parse, desc='Apartments', position=0, leave=False):
-            for j, url_apartment in enumerate(urls_apartments_to_parse):
-                print('{0} / {1}'.format(j + 1, len(urls_apartments_to_parse)))
+            for url_apartment in tqdm(urls_apartments_to_parse, desc='Apartments', leave=False, ascii=True):
+                df = df.append(parse_apartment(url_apartment), ignore_index=True)
 
-                storage_dict[url_apartment] = parse_apartment(url_apartment)
-
-    with open(full_filename, 'w') as file:
-        json.dump(storage_dict, file, ensure_ascii=False)
-    print('Done!')
-    print('New apartments:', len(storage_dict) - len_storage)
+    if not df.empty:
+        df.rename(columns=rename_map, inplace=True)
+        df.to_sql(name='Apartments', con=engine, schema='dbo', if_exists='append', index=False)
+    print('New Apartments:', len(df))
 
 
 if __name__ == '__main__':
